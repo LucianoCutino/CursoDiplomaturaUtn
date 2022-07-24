29beecf8440b9a6dc77d7ea4f8137396218a8056
Luciano Cutino <lucianocutino@gmail.com>
2022-07-24 15:04:12 -0300

    corregi el menu responsive

--- tc3000/frontend/src/components/layout/Header.js
+++ tc3000/frontend/src/components/layout/Header.js
@@ -1,31 +1,42 @@
 import '../../styles/Layout.css';
 
+import { useRef } from 'react';
+
 import { Link } from "react-router-dom";
 
 const Header = (props) => {
+
+    const checkbox = useRef(null);
+
+    const handleItemClick = e => {
+        if (checkbox.current) {
+            checkbox.current.checked = false;
+        }
+    }
+
     return (
         <header>
-        <nav>
-            <div className="holder">
-                <input type="checkbox" id="check"/>
-                <label for="check" class="checkbtn">
-                    <i class="fa-solid fa-bars"></i>
-                </label>
-                <a href="/" class="enlance">
-                    <img src="img/Logo3.png" alt="" class="logo"/>
-                </a>
-                <ul>
-                    <li><Link to="/">Inicio</Link></li>
-                    <li><Link to="/Institucional">Institucional</Link></li>
-                    <li><Link to="/Pilotos">Pilotos</Link></li>
-                    <li><Link to="/Calendario">Calendario</Link></li>
-                    <li><Link to="/Fotos">Fotos</Link></li>
-                    <li><Link to="/Novedades">Novedades</Link></li>
-                    <li><Link to="/Contacto">Contacto</Link></li>
-                    
-                </ul>
-            </div>
-        </nav>
+            <nav>
+                <div className="holder">
+                    <input type="checkbox" id="check" ref={checkbox} />
+                    <label for="check" class="checkbtn">
+                        <i class="fa-solid fa-bars"></i>
+                    </label>
+                    <a href="/" class="enlance">
+                        <img src="img/Logo3.png" alt="" class="logo" />
+                    </a>
+                    <ul>
+                        <li><Link to="/" onClick={handleItemClick}>Inicio</Link></li>
+                        <li><Link to="/Institucional" onClick={handleItemClick}>Institucional</Link></li>
+                        <li><Link to="/Pilotos" onClick={handleItemClick}>Pilotos</Link></li>
+                        <li><Link to="/Calendario" onClick={handleItemClick}>Calendario</Link></li>
+                        <li><Link to="/Fotos" onClick={handleItemClick}>Fotos</Link></li>
+                        <li><Link to="/Novedades" onClick={handleItemClick}>Novedades</Link></li>
+                        <li><Link to="/Contacto" onClick={handleItemClick}>Contacto</Link></li>
+
+                    </ul>
+                </div>
+            </nav>
         </header>
     );
 }
